4e81bac76ff4487d26e0b31b3a9b8598e429aca3
Ean Houts <ehouts@pacbell.net>
2011-12-06 21:54:15 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeedTrap::Application.config.secret_token = 'f8ae2bb55e4ac5d5ff900b596a2a5fae392d8eca0cedd0c172eb836b3f7a82ab43432730f5d924c8c25eeba819b84f544fe01240fe400f303aec41bc0eff8257'
